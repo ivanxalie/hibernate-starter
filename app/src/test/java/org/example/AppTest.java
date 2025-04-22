47ed0745e3a2a06b5f3ddbb774e0869e80f12bf3
ivanxalie <ivanxalie@gmail.com>
2025-04-22 22:22:10 +0300

    Add hibernate, postgres driver, lombok

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -1,10 +1,9 @@
 package org.example;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+
 class AppTest {
 
     @Test
