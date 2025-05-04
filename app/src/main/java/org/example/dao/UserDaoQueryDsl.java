de386ff1d7212b6d4263c9e8071fb8e7d9f72718
ivanxalie <ivanxalie@gmail.com>
2025-05-04 19:26:01 +0300

    Almost the whole org.example.dao.UserDaoQueryDsl implemented

--- app/src/main/java/org/example/dao/UserDaoQueryDsl.java
+++ app/src/main/java/org/example/dao/UserDaoQueryDsl.java
@@ -4,6 +4,7 @@ import com.querydsl.jpa.impl.JPAQuery;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.example.entity.Payment;
+import org.example.entity.QPayment;
 import org.example.entity.QUser;
 import org.example.entity.User;
 import org.hibernate.Session;
@@ -28,26 +29,51 @@ public class UserDaoQueryDsl implements UserDao {
 
     @Override
     public List<User> findAllByFirstName(Session session, String firstName) {
-        return List.of();
+        return new JPAQuery<User>(session)
+                .select(QUser.user)
+                .from(QUser.user)
+                .where(QUser.user.personalInfo.firstName.eq(firstName))
+                .fetch();
     }
 
     @Override
     public List<User> findLimitedUsersOrderedByBirthday(Session session, int limit) {
-        return List.of();
+        return new JPAQuery<User>(session)
+                .select(QUser.user)
+                .from(QUser.user)
+                .limit(limit)
+                .orderBy(QUser.user.personalInfo.birthDate.asc())
+                .fetch();
     }
 
     @Override
     public List<User> findAllByCompanyName(Session session, String companyName) {
-        return List.of();
+        return new JPAQuery<User>(session)
+                .select(QUser.user)
+                .from(QUser.user)
+                .where(QUser.user.company.name.eq(companyName))
+                .fetch();
     }
 
     @Override
     public List<Payment> findAllPaymentsByCompanyName(Session session, String companyName) {
-        return List.of();
+        return new JPAQuery<Payment>(session)
+                .select(QPayment.payment)
+                .from(QPayment.payment)
+                .where(QPayment.payment.receiver.company.name.eq(companyName))
+                .fetch();
     }
 
     @Override
     public Double findAveragePaymentAmountByFirstAndLastNames(Session session, String firstName, String lastName) {
-        return 0.0;
+        return new JPAQuery<Double>(session)
+                .select(QPayment.payment.amount.avg())
+                .from(QPayment.payment)
+                .where(
+                        QPayment.payment.receiver.personalInfo.firstName.eq(firstName),
+                        QPayment.payment.receiver.personalInfo.lastName.eq(lastName)
+                )
+                .groupBy(QPayment.payment.receiver.id)
+                .fetchOne();
     }
 }
