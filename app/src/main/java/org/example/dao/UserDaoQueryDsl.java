3a1b457530d6c862199b42fccb46bf52ee3c1ec9
ivanxalie <ivanxalie@gmail.com>
2025-05-04 19:27:47 +0300

    Refactoring: add static import in org.example.dao.UserDaoQueryDsl for QEntities

--- app/src/main/java/org/example/dao/UserDaoQueryDsl.java
+++ app/src/main/java/org/example/dao/UserDaoQueryDsl.java
@@ -4,12 +4,13 @@ import com.querydsl.jpa.impl.JPAQuery;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.example.entity.Payment;
-import org.example.entity.QPayment;
-import org.example.entity.QUser;
 import org.example.entity.User;
 import org.hibernate.Session;
 
 import java.util.List;
+
+import static org.example.entity.QPayment.payment;
+import static org.example.entity.QUser.user;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserDaoQueryDsl implements UserDao {
@@ -22,58 +23,58 @@ public class UserDaoQueryDsl implements UserDao {
     @Override
     public List<User> findAll(Session session) {
         return new JPAQuery<User>(session)
-                .select(QUser.user)
-                .from(QUser.user)
+                .select(user)
+                .from(user)
                 .fetch();
     }
 
     @Override
     public List<User> findAllByFirstName(Session session, String firstName) {
         return new JPAQuery<User>(session)
-                .select(QUser.user)
-                .from(QUser.user)
-                .where(QUser.user.personalInfo.firstName.eq(firstName))
+                .select(user)
+                .from(user)
+                .where(user.personalInfo.firstName.eq(firstName))
                 .fetch();
     }
 
     @Override
     public List<User> findLimitedUsersOrderedByBirthday(Session session, int limit) {
         return new JPAQuery<User>(session)
-                .select(QUser.user)
-                .from(QUser.user)
+                .select(user)
+                .from(user)
                 .limit(limit)
-                .orderBy(QUser.user.personalInfo.birthDate.asc())
+                .orderBy(user.personalInfo.birthDate.asc())
                 .fetch();
     }
 
     @Override
     public List<User> findAllByCompanyName(Session session, String companyName) {
         return new JPAQuery<User>(session)
-                .select(QUser.user)
-                .from(QUser.user)
-                .where(QUser.user.company.name.eq(companyName))
+                .select(user)
+                .from(user)
+                .where(user.company.name.eq(companyName))
                 .fetch();
     }
 
     @Override
     public List<Payment> findAllPaymentsByCompanyName(Session session, String companyName) {
         return new JPAQuery<Payment>(session)
-                .select(QPayment.payment)
-                .from(QPayment.payment)
-                .where(QPayment.payment.receiver.company.name.eq(companyName))
+                .select(payment)
+                .from(payment)
+                .where(payment.receiver.company.name.eq(companyName))
                 .fetch();
     }
 
     @Override
     public Double findAveragePaymentAmountByFirstAndLastNames(Session session, String firstName, String lastName) {
         return new JPAQuery<Double>(session)
-                .select(QPayment.payment.amount.avg())
-                .from(QPayment.payment)
+                .select(payment.amount.avg())
+                .from(payment)
                 .where(
-                        QPayment.payment.receiver.personalInfo.firstName.eq(firstName),
-                        QPayment.payment.receiver.personalInfo.lastName.eq(lastName)
+                        payment.receiver.personalInfo.firstName.eq(firstName),
+                        payment.receiver.personalInfo.lastName.eq(lastName)
                 )
-                .groupBy(QPayment.payment.receiver.id)
+                .groupBy(payment.receiver.id)
                 .fetchOne();
     }
 }
